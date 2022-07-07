a049708c92a5433d0005aa44ed6a27edfa5afcaf
Vinh <ladykillah962@gmail.com>
2022-07-07 09:56:26 +0700

    update detail product

--- src/features/Product/index.jsx
+++ src/features/Product/index.jsx
@@ -1,16 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import { Route, Routes } from 'react-router-dom';
-import ProductListPage from './pages/ProductListPage';
 import ProductDetailPage from './pages/ProductDetailPage';
-
-ProductFeature.propTypes = {};
+import ProductListPage from './pages/ProductListPage';
 
 function ProductFeature(props) {
   return (
     <Routes>
       <Route path='/' element={<ProductListPage />} />
-      <Route path='/:productDetailID' element={<ProductDetailPage />} />
+      <Route path='/:productDetailID/*' element={<ProductDetailPage />} />
     </Routes>
   );
 }
